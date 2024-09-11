f092ecedc5e7fa375befddc7134458a7923efb82
Elakkiya0621 <Elakkiyamurugesan21@gmail.com>
2024-09-11 18:02:10 +0530

    Elakkiya's code

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -3,5 +3,8 @@ package org.sample;
 public class Sample {
 public static void main(String[] args) {
 	System.out.println("Test");
+	System.out.println("Test1");
+	System.out.println("Test2");
+	System.out.println("Test3");
 }
 }
